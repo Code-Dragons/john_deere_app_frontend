82cfd20aca4db03e65b3575d3872ab279b6243b7
Felistas <felistas.ngumi@andela.com>
2018-04-10 23:05:33 +0300

    implement landing page

--- john-deere-frontend/src/components/LandingPage/LandingPage.component.jsx
+++ john-deere-frontend/src/components/LandingPage/LandingPage.component.jsx
@@ -4,7 +4,21 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>here</h1>
+        <div className="header">
+          <div className="header-top-content">
+            <div className="header-top-content-login">
+              <h2>LOGIN</h2>
+            </div>
+            <h2>REGISTER</h2>
+          </div>
+          <div className="header-center-content">
+            <p>
+              A BETTER WAY TO GO ABOUT YOUR FARMING ACTIVITIES. <br />OWN A
+              TRACTOR INDIVIDUALLY OR AS A GROUP
+            </p>
+            <div className="header-button">GET STARTED</div>
+          </div>
+        </div>
       </div>
     );
   }
